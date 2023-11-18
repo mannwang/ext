4ef94728177e1c8687b38679aa83c96d3b136003
wangmanxf <wangmanxf@gmail.com>
2023-11-18 22:32:07 +0800

    feat: code align

--- evp_ccm.h
+++ evp_ccm.h
@@ -6,16 +6,16 @@
 #define _EVP_CCM_H_
 
 int ccm_encrypt(unsigned char *plaintext, int plaintext_len,
-                unsigned char *aad, int aad_len,
-                unsigned char *key,
-                unsigned char *iv,
-                unsigned char *ciphertext,
-                unsigned char *tag);
+    unsigned char *aad, int aad_len,
+    unsigned char *key,
+    unsigned char *iv,
+    unsigned char *ciphertext,
+    unsigned char *tag);
 int ccm_decrypt(unsigned char *ciphertext, int ciphertext_len,
-                unsigned char *aad, int aad_len,
-                unsigned char *tag,
-                unsigned char *key,
-                unsigned char *iv,
-                unsigned char *plaintext);
+    unsigned char *aad, int aad_len,
+    unsigned char *tag,
+    unsigned char *key,
+    unsigned char *iv,
+    unsigned char *plaintext);
 
 #endif
--- evp_gcm.h
+++ evp_gcm.h
@@ -6,16 +6,16 @@
 #define _EVP_GCM_H_
 
 int gcm_encrypt(unsigned char *plaintext, int plaintext_len,
-                unsigned char *aad, int aad_len,
-                unsigned char *key,
-                unsigned char *iv, int iv_len,
-                unsigned char *ciphertext,
-                unsigned char *tag);
+    unsigned char *aad, int aad_len,
+    unsigned char *key,
+    unsigned char *iv, int iv_len,
+    unsigned char *ciphertext,
+    unsigned char *tag);
 int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
-                unsigned char *aad, int aad_len,
-                unsigned char *tag,
-                unsigned char *key,
-                unsigned char *iv, int iv_len,
-                unsigned char *plaintext);
+    unsigned char *aad, int aad_len,
+    unsigned char *tag,
+    unsigned char *key,
+    unsigned char *iv, int iv_len,
+    unsigned char *plaintext);
 
 #endif
